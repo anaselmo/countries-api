1ede7b0359da6cb9f7580f1c88bafab0d8c2d914
nachoceltiberian <ignacio.moya@celtiberian.es>
2024-02-16 20:31:02 +0100

    comments deleted

--- src/routes/tourists.router.ts
+++ src/routes/tourists.router.ts
@@ -5,36 +5,16 @@ import { handleError } from '../utils/handleError'
 
 const router = Router()
 
-/**
- * Obtener la lista de todos los turistas
- */
 router.get('/', handleError(getTourists))
 
-/**
- * Obtener la información de un turista por su id
- */
 router.get('/:id', handleError(getTourist))
 
-/**
- * Registrarse como turista
- */
 router.post('/auth/register', handleError(registerTourist))
 
-/**
- * Iniciar sesión como turista
- */
 router.post('/auth/login', handleError(loginTourist))
 
-/**
- * Actualizar información del turista del token
- * @pre debe tener un token
- */
 router.put('/', authMiddleware, handleError(updateTourist))
 
-/**
- * Eliminar turista del token
- * @pre debe tener un token
- */
 router.delete('/', authMiddleware, handleError(deleteTourist))
 
 export default router
