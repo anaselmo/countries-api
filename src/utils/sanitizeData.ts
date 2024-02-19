75ec15ced9853be50a62b11f3e1423ce3660270f
nachoceltiberian <ignacio.moya@celtiberian.es>
2024-02-19 11:31:21 +0100

    specific functions for each entity

--- src/utils/sanitizeData.ts
+++ src/utils/sanitizeData.ts
@@ -1,6 +1,23 @@
+import type { Tourist, Country, Visit } from '@prisma/client'
+import { type TouristOutputDto } from '../dtos/touristService.dto'
+import { type CountryOutputDto } from '../dtos/countryService.dto'
+import { type VisitOutputDto } from '../dtos/visitOutput.dto'
+
 export const sanitizeData = <T, K extends keyof T>(data: T, ...propertiesToDelete: K[]): Omit<T, K> => {
   const sanitizedData = { ...data }
   // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
   propertiesToDelete.forEach(property => delete sanitizedData[property])
   return sanitizedData
+}
+
+export const sanitizeTourist = (tourist: Tourist): TouristOutputDto => {
+  return sanitizeData(tourist, 'deleted', 'password')
+}
+
+export const sanitizeCountry = (country: Country): CountryOutputDto => {
+  return sanitizeData(country, 'deleted')
+}
+
+export const sanitizeVisit = (visit: Visit): VisitOutputDto => {
+  return sanitizeData(visit, 'deleted')
 }
