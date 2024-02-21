5dd170fb4dabdbf7cea7773151e5153c947045ea
anaselmo <nacho.moya.montoro@gmail.com>
2024-02-21 11:32:50 +0100

    added get and upsert from external API

--- src/services/countries.service.ts
+++ src/services/countries.service.ts
@@ -4,10 +4,27 @@ import { AlreadyExistsError, NotFoundError } from '../utils/handleError'
 import type { CreateCountryDto } from '../dtos/createCountry.dto'
 import type { UpdateCountryDto } from '../dtos/updateCountry.dto'
 import type { CountryOutputDto, ICountryService } from '../dtos/countryService.dto'
-import { sanitizeCountry } from '../utils/sanitizeData'
+import { sanitizeCountry, sanitizeCountryAPI } from '../utils/sanitizeData'
+import { ExternalService } from './external.service'
+
+const countriesApiService = new ExternalService('https://api.sampleapis.com/countries/countries')
 
 export class CountryService implements ICountryService {
   constructor (private readonly repo: typeof prisma) {}
+
+  public async getExternalCountries (): Promise<CreateCountryDto[]> {
+    return (
+      (await countriesApiService.getData(''))
+        .map(country => sanitizeCountryAPI(country))
+    )
+  }
+
+  public async upsertCountriesFromAPI (): Promise<CountryOutputDto[]> {
+    const countries = (await countriesApiService.getData(''))
+    const sanitizedCountries = countries.map(country => sanitizeCountryAPI(country))
+    const upsertedCountries = await Promise.all(sanitizedCountries.map(async country => await this.upsertCountry(country)))
+    return upsertedCountries
+  }
 
   public async getCountry (id: Country['id']): Promise<CountryOutputDto> {
     const country = await this.repo.country.findFirst({
@@ -58,6 +75,23 @@ export class CountryService implements ICountryService {
     })
 
     return sanitizeCountry(updatedCountry)
+  }
+
+  public async upsertCountry (data: CreateCountryDto): Promise<CountryOutputDto> {
+    try {
+      return sanitizeCountry(await this.repo.country.upsert({
+        where: { abbreviation: data.abbreviation },
+        create: data,
+        update: data
+      }))
+    } catch (err) {
+      if (err instanceof Prisma.PrismaClientKnownRequestError) {
+        if (err.code === 'P2002') {
+          throw new AlreadyExistsError(`Country with name "${data.name}" or abbreviation "${data.abbreviation}" already exists`)
+        }
+      }
+      throw err
+    }
   }
 
   public async createCountry (data: CreateCountryDto): Promise<CountryOutputDto> {
