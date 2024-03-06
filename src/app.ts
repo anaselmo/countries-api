a11a84fd2aced15656a410d3e583088c06bbe91d
anaselmo <nacho.moya.montoro@gmail.com>
2024-03-06 12:38:43 +0100

    order of app.use() changed

--- src/app.ts
+++ src/app.ts
@@ -5,14 +5,13 @@ import { errorMiddleware } from './middleware/errors.middleware'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const app = express()
+const PORT = process.env.PORT
 
+const app = express()
 app.use(express.json())
 app.use(cors())
-app.use(errorMiddleware)
 app.use('/api', router)
-
-const PORT = process.env.PORT
+app.use(errorMiddleware)
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`)
